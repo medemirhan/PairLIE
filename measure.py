7c180e214ab908aac3083e5c3e76b00a929267c4
medemirhan <mahmutesat@gmail.com>
2024-05-21 00:45:17 +0300

    sam added to metrics

--- measure.py
+++ measure.py
@@ -4,58 +4,38 @@ import torch
 import glob
 import numpy as np
 from PIL import Image
-from torchmetrics.image import PeakSignalNoiseRatio, StructuralSimilarityIndexMeasure
+from torchmetrics.image import PeakSignalNoiseRatio, StructuralSimilarityIndexMeasure, SpectralAngleMapper
 import torch
+import utils
 
 def ssim(input, target, data_range=None):
-
-    if torch.is_tensor(input):
-        data_in = input
-    else:
-        data_in = torch.tensor(input)
-
-    if torch.is_tensor(target):
-        data_out = target
-    else:
-        data_out = torch.tensor(target)
-
     if data_range == None:
-        psnr_torch = StructuralSimilarityIndexMeasure()
+        ssim_torch = StructuralSimilarityIndexMeasure()
     else:
-        psnr_torch = StructuralSimilarityIndexMeasure(data_range=data_range)
+        ssim_torch = StructuralSimilarityIndexMeasure(data_range=data_range)
 
-    return psnr_torch(data_in, data_out)
+    return ssim_torch(input, target)
 
 def psnr(input, target, data_range=None):
-
-    if torch.is_tensor(input):
-        data_in = input
-    else:
-        data_in = torch.tensor(input)
-
-    if torch.is_tensor(target):
-        data_out = target
-    else:
-        data_out = torch.tensor(target)
-
     if data_range == None:
         psnr_torch = PeakSignalNoiseRatio()
     else:
         psnr_torch = PeakSignalNoiseRatio(data_range=data_range)
 
-    return psnr_torch(data_in, data_out)
+    return psnr_torch(input, target)
+
+def sam(input, target, reduction='elementwise_mean'):
+    sam_torch = SpectralAngleMapper(reduction=reduction)
+    return sam_torch(input, target)
 
 def metrics(im_dir, label_dir):
     avg_psnr = 0
     avg_ssim = 0
     n = 0
-
     for item in sorted(glob.glob(im_dir)):
         n += 1
         im1 = Image.open(item).convert('RGB') 
-
         name = item.split('\\')[-1]
-
         im2 = Image.open(os.path.join(label_dir, name)).convert('RGB')
         (h, w) = im2.size
         im1 = im1.resize((h, w))  
@@ -66,8 +46,7 @@ def metrics(im_dir, label_dir):
         im2 = torch.tensor(im2).unsqueeze(0).permute(0, 3, 1, 2)
 
         score_psnr = psnr(im1, im2)
-        ss = StructuralSimilarityIndexMeasure()
-        score_ssim = ss(im1, im2)
+        score_ssim = ssim(im1, im2)
     
         avg_psnr += score_psnr
         avg_ssim += score_ssim
@@ -77,14 +56,43 @@ def metrics(im_dir, label_dir):
 
     return avg_psnr, avg_ssim
 
+def metrics_hsi(im_dir, label_dir):
+    avg_psnr = 0
+    avg_ssim = 0
+    avg_sam = 0
+    n = 0
+    for item in sorted(glob.glob(im_dir)):
+        n += 1
+        im1 = utils.load_hsi_as_tensor(item)
+        name = item.split('\\')[-1]
+        im2 = utils.load_hsi_as_tensor(os.path.join(label_dir, name))
+
+        im1 = im1.unsqueeze(0)
+        im2 = im2.unsqueeze(0)
+
+        score_psnr = psnr(im1, im2, data_range=None) # data range onemli. incele!
+        score_ssim = ssim(im1, im2, data_range=None) # data range onemli. incele!
+        score_sam = sam(im1, im2, reduction='elementwise_mean') # reduction onemli. incele!
+    
+        avg_psnr += score_psnr
+        avg_ssim += score_ssim
+        avg_sam += score_sam
+
+    avg_psnr = avg_psnr / n
+    avg_ssim = avg_ssim / n
+    avg_sam = avg_sam / n
+
+    return avg_psnr, avg_ssim, avg_sam
+
 if __name__ == '__main__':
 
-    im_dir = 'PairLIE-our-results/LOL-test/I/*.png'
-    label_dir = 'PairLIE-testing-dataset/LOL-test/reference'
+    #im_dir = 'PairLIE-our-results/LOL-test/I/*.png'
+    #label_dir = 'PairLIE-testing-dataset/LOL-test/reference'
 
-    #im_dir = 'results/sonuc/*.mat'
-    #label_dir = 'test_reference'
+    im_dir = 'results/sonuc/deneme/*.mat'
+    label_dir = 'test_reference/deneme'
 
-    avg_psnr, avg_ssim = metrics(os.path.normpath(im_dir), os.path.normpath(label_dir))
-    print("===> Avg.PSNR: {:.4f} dB ".format(avg_psnr))
-    print("===> Avg.SSIM: {:.4f} ".format(avg_ssim))
+    avg_psnr, avg_ssim, avg_sam = metrics_hsi(os.path.normpath(im_dir), os.path.normpath(label_dir))
+    print("===> Avg.PSNR : {:.4f} dB ".format(avg_psnr))
+    print("===> Avg.SSIM : {:.4f} ".format(avg_ssim))
+    print("===> Avg.SAM  : {:.4f} ".format(avg_sam))
